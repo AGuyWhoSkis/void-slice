f74d8d3099493752e16bf3f4f2a61ab18f194932
Trevor Incze <trevor.incze@hotmail.com>
2026-02-10 00:24:50 -0500

    (scanner): add token assertions

--- internal/scan/test/scan_test.go
+++ internal/scan/test/scan_test.go
@@ -13,10 +13,11 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+var testDir = filepath.FromSlash("../../../void-files/d2/")
+
 var ingestedFiles map[string]string
 var filesToIngest = []string{
 	"game1/generated.decls.gamelogicmanager.ui.gamelogic.manager..gamelogicmanager.decl",
-	// "game1/generated.decls.activeragdoll.models.characters.base.active_ragdoll.ar_hookmine_head_ceiling..activeragdoll.decl",
 	"game1/generated.decls.cpntplayerfxmanager.components.characters.player.base.fx_manager..cpntplayerfxmanager.decl",
 	"game1/generated.decls.greatestmomentsmanager.greatestmoments.manager.manager..greatestmomentsmanager.decl",
 	"game1/maps.campaign.dunwall.escape.tower.dunwall_escape_tower_p.entities",
@@ -25,14 +26,13 @@ var filesToIngest = []string{
 func TestMain(m *testing.M) {
 	flag.Parse()
 
-	testDir := filepath.FromSlash("../../../void-files/d2/")
 	ingestedFiles = make(map[string]string)
 
 	for _, filename := range filesToIngest {
-		fp := filepath.Join(testDir, filename)
-		b, err := os.ReadFile(fp)
+		filePath := filepath.Join(testDir, filename)
+		b, err := os.ReadFile(filePath)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "test setup failed: read %q: %v\n", fp, err)
+			fmt.Fprintf(os.Stderr, "test setup failed: read %q: %v\n", filePath, err)
 			os.Exit(1)
 		}
 		ingestedFiles[filename] = string(b)
@@ -50,28 +50,45 @@ func TestScanner(t *testing.T) {
 		name      string
 		bytes     []byte
 		wantDiags []scan.Diagnostic
+		wantToks  []scan.Token
 	}
 
 	file0 := ingestedFiles[filesToIngest[0]]
-	// file1 := ingestedFiles[filesToIngest[1]]
+	file1 := ingestedFiles[filesToIngest[1]]
+	file2 := ingestedFiles[filesToIngest[2]]
 
 	require.NotEmpty(t, file0, "expected non-empty test file (file0)")
+	require.NotEmpty(t, file1, "expected non-empty test file (file1)")
+	require.NotEmpty(t, file2, "expected non-empty test file (file2)")
 
 	tests := []tc{
-		// {
-		// 	name:      "sanity test",
-		// 	bytes:     []byte("\n{\n\tedit = {\n\t\n\t}\n}"),
-		// 	wantDiags: []scan.Diagnostic{}, // zero scanner errors should be produced
-		// },
-		// {
-		// 	name:      "file0",
-		// 	bytes:     []byte(file0),
-		// 	wantDiags: []scan.Diagnostic{},
-		// },
+		{
+			name:      "valid snippet",
+			bytes:     []byte("\n{\n\tedit = {\n\t\n\t}\n}"),
+			wantDiags: []scan.Diagnostic{},
+		},
+		{
+			name:  "unterminated-quote",
+			bytes: []byte(`{ edit = { m_max = "missing-right-hand-quote ; } }`), // len=50; final byte index=49
+			wantDiags: []scan.Diagnostic{
+				{
+					Code:     scan.Codes.SCAN,
+					Severity: scan.Severities.ERROR,
+					Span:     scan.NewSpan(19, 50),
+					Message:  "unterminated quote",
+				},
+			},
+		},
 		{
 			name:      "giant .entities file",
 			bytes:     []byte(ingestedFiles[filesToIngest[3]]),
 			wantDiags: []scan.Diagnostic{},
+			wantToks: []scan.Token{
+				{
+					Kind: scan.TokenKind.QUOTE_LITERAL,
+					Span: scan.NewSpan(, end int),
+				},
+			},
 		},
 	}
 
@@ -88,6 +105,16 @@ func TestScanner(t *testing.T) {
 			assert.ElementsMatch(t, tt.wantDiags, result_diags,
 				"expected Diagnostics does not match actual Dignostics:\n"+
 					"\tExpected\n%v\n\tActual\n%v\n", tt.wantDiags, result_diags)
+
+			if tt.wantToks != nil {
+				for i, wantToken := range tt.wantToks {
+					require.NotNil(t, wantToken, "how the heck is this nil anyway (wantToken %d)", i)
+					if wantToken.Span.Start != 0 || wantToken.Span.End != 0 {
+						// the test defined Spans to assert
+						assert.Containsf(t, result_tokens, wantToken, "test %s wants this token, but it was missing from Scanner output: %v", t.Name(), wantToken)
+					}
+				}
+			}
 
 			timeMillis := endTime.Sub(startTime).Milliseconds()
 			fmt.Printf("%s\nconsumed %d bytes, produced %d tokens and %d diags\t(took %dms)", t.Name(), len(tt.bytes), len(result_tokens), len(result_diags), timeMillis)
